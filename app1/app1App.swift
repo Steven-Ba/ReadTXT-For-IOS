8e5b6de34b976ac16a32c465e031ff8b67e945b5
StevenE.Harrington <brj691208@gmail.com>
2026-03-29 13:04:38 +0800

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Steven Edward Harrington on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
